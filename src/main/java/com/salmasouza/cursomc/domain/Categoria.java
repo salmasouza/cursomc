29c6024287fb0e12f59d5032d6bda5ca388984a2
Salma Souza <salma.souzza@gmail.com>
2022-10-07 14:45:12 -0300

    Criando Operação de Instanciação

--- src/main/java/com/salmasouza/cursomc/domain/Categoria.java
+++ src/main/java/com/salmasouza/cursomc/domain/Categoria.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 
 
@@ -14,6 +15,8 @@ import javax.persistence.Id;
 @Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
+	
+	
 	
 	@Id
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
